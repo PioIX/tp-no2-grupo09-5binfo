066229935027eea3f3e0bd6f2e92410d0059cfaf
rocio5to <48994590+rocio5to@users.noreply.github.com>
2019-06-05 11:20:42 -0300

    Add files via upload

---
+++ github.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitHubBingo
+{
+    public partial class BINGO : Form
+    {
+        public BINGO()
+        {
+            InitializeComponent();
+        }
+
+        private void BINGO_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "2";
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "3";
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "4";
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "5";
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "6";
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "7";
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "8";
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "9";
+
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "10";
+
+        }
+        // 20
+        private void btn11_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "11";
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "12";
+
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "13";
+
+        }
+
+        private void btn14_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "14";
+
+        }
+
+        private void btn15_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "15";
+
+        }
+
+        private void btn16_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "16";
+
+        }
+
+        private void btn17_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "17";
+
+        }
+
+        private void btn18_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "18";
+
+        }
+
+        private void btn19_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "19";
+
+        }
+
+        private void btn20_Click(object sender, EventArgs e)
+        {
+            btnSalio.Text = "20";
+
+        }
+
+    }
+}
